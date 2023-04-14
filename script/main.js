5fdeab4a2b99ae392175f9540b2e6d0185b72393
roberjunnior <roberjunior.dev@gmail.com>
2023-04-14 01:36:50 -0300

    update

--- script/main.js
+++ script/main.js
@@ -1,3 +1,23 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
 
-document.querySelector('#som_tecla_pom').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+    // console.log(idAudio);
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    // console.log(contador);
+}
