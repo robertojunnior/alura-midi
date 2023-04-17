a4171b1c91b1fb9b211f5ce7bda04138df63d1b4
roberjunnior <roberjunior.dev@gmail.com>
2023-04-16 23:13:11 -0300

    update

--- script/main.js
+++ script/main.js
@@ -5,12 +5,13 @@ function tocaSom (idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-
+//enquanto
 while (contador < listaDeTeclas.length) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
+    
     // console.log(idAudio);
     
     tecla.onclick = function () {
